796876b129130aac9c39f73bdd860072ca1a3e03
Ishansh10113 <ishansh.bhardwaj10113@gmail.com>
2025-05-10 08:42:22 +0530

    updated code

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OpulRent.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EquiTerra.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
